c3f7b4ed4fcdcf3c43ae7a85a72997a7cd0a3298
pavalucas <lucasapava@gmail.com>
2017-06-07 14:22:47 -0300

    Added function that reads and parses entry file

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAXN 1000
+
+int readLogFile(char* fileName, unsigned int addr[], char rw[])
+{
+	FILE* fp = fopen(fileName, "r");
+	int i = 0;
+	while(fscanf(fp, "%x %c", &addr[i], &rw[i]) == 2)
+		i++;
+	return i;
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc < 5)
-    {
-        printf("Error! 5 parameters are necessary!\n");
-        exit(1);
-    }
+	if (argc < 5)
+  {
+  	printf("Error! 5 parameters are necessary!\n");
+  	exit(1);
+  }
 
-    char* replacementAlg = argv[1];
-    char* adressFile = argv[2];
-    int pageSize = atoi(argv[3]);
-    int physicalMemorySize = atoi(argv[4]);
+  char* replacementAlg = argv[1];
+  char* adressFile = argv[2];
+  int pageSize = atoi(argv[3]);
+  int physicalMemorySize = atoi(argv[4]);
+	unsigned int addr[MAXN];
+	char rw[MAXN];
+	int i;
+	int numberOfAddr;
 
+	for(i = 0; i < argc; i++)
+		printf("%s ", argv[i]);
+	printf("\n");
+
+	numberOfAddr = readLogFile(adressFile, addr, rw);
+
+	printf("Number of adresses: %d\n", numberOfAddr);
+	for(i = 0; i < numberOfAddr; i++)
+	{
+		printf("%x %c\n", addr[i], rw[i]);
+	}
 }
