bd3f326eb89f650b7b0414c6dd0f0ff052bc380b
pavalucas <lucasapava@gmail.com>
2017-06-21 01:07:29 -0300

    Added first version of LRU algorithm. Also added debug compiler option

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #define MAXN 1000
 
-typedef struct 
+typedef struct
 {
 	char R;
 	char M;
@@ -37,11 +38,126 @@ int getPageFrameSize(int pageSize, int physMemSize)
 	return ((physMemSize * 1024) / pageSize);
 }
 
+void LRU(int* pageTable, PageFrame* pageFrame, int pageFrameSize, int pageSize, int deltaTime, FILE* fp)
+{
+	unsigned int addr;
+	char rw;
+	int i;
+	int qtPageFault = 0;
+	int qtPhysMemAcess = 0;
+	int pagesEmpty = pageFrameSize;
+	unsigned long long time = 1;
+	unsigned int* ageArr = (unsigned int*)malloc(sizeof(unsigned int) * pageFrameSize);
+	memset(ageArr, 0, sizeof(ageArr));
+
+	while(fscanf(fp, "%x %c", &addr, &rw) == 2)
+	{
+		int newPageIndex = getPageIndex(addr, pageSize);
+		int indexNewPageFrame = pageTable[newPageIndex];
+		int oldPageIndex;
+
+		PageFrame newPage;
+
+		// Setting all R bit to zero
+		if(time % deltaTime == 0)
+		{
+			#ifdef _DEBUG
+				printf("Atualizando os valor do vetor de envelhecimento.\n");
+			#endif
+			for(i = 0; i < pageFrameSize; i++)
+			{
+				ageArr[i] >>= 1;
+				if(pageFrame[i].R == 1)
+				{
+					ageArr[i] |= (1 << 31);
+					pageFrame[i].R = 0;
+				}
+				#ifdef _DEBUG
+					//printf("ageArr[%d] -> %u\n", i, ageArr[i]);
+				#endif
+			}
+		}
+
+		if(indexNewPageFrame == -1)
+		{
+			if(pagesEmpty != 0)
+			{
+				newPage.lastAcessed = time;
+
+				if(rw == 'W')
+					newPage.M = 1;
+				else
+					newPage.M = 0;
+				newPage.R = 1;
+
+				oldPageIndex = pageFrameSize - pagesEmpty;
+				pagesEmpty--;
+				pageFrame[oldPageIndex] = newPage;
+				pageTable[newPageIndex] = oldPageIndex;
+				#ifdef _DEBUG
+					printf("Nova page frame %d preenchida\n", oldPageIndex);
+				#endif
+			}
+			else
+			{
+				int minValue = INT_MAX;
+				int minPos;
+				for(i = 0; i < pageFrameSize; i++)
+				{
+					if(ageArr[i] < minValue)
+					{
+						minValue = ageArr[i];
+						minPos = i;
+					}
+				}
+				#ifdef _DEBUG
+					printf("Page fault!\n");
+					printf("Pagina %d sera retirada.\n", minPos);
+				#endif
+				if(pageFrame[minPos].M == 1)
+				{
+					#ifdef _DEBUG
+						printf("Acesso a memoria fisica!\n");
+					#endif
+					qtPhysMemAcess++;
+				}
+
+				if(rw == 'W')
+					pageFrame[minPos].M = 1;
+				else
+					pageFrame[minPos].M = 0;
+				pageFrame[minPos].R = 1;
+				pageFrame[minPos].lastAcessed = time;
+
+				pageTable[newPageIndex] = minPos;
+
+				qtPageFault++;
+			}
+		}
+		else
+		{
+			#ifdef _DEBUG
+				printf("Page %d hit!\n", indexNewPageFrame);
+			#endif
+
+			if(rw == 'W')
+				pageFrame[indexNewPageFrame].M = 1;
+			pageFrame[indexNewPageFrame].R = 1;
+
+			pageFrame[indexNewPageFrame].lastAcessed = time;
+		}
+
+		time++;
+	}
+	printf("Numero de Faltas de Páginas: %d\n", qtPageFault);
+	printf("Numero de Paginas escritas: %d\n", qtPhysMemAcess);
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 5)
 	{
-		printf("Error! 5 parameters are necessary!\n");
+		printf("Erro! 5 parametros são necessários!\n");
 		exit(1);
 	}
 
@@ -49,21 +165,26 @@ int main(int argc, char *argv[])
 	char* addressFile = argv[2];
 	int pageSize = atoi(argv[3]);
 	int physicalMemorySize = atoi(argv[4]);
-	
+
 	unsigned int addr;
 	char rw;
 	int i;
 	int numberOfAddr;
 	int* pageTable;
 	PageFrame* pageFrame;
-	unsigned long long time = 0;
+	unsigned long long time = 1;
+	unsigned long long deltaTime = 5;
+
+	#ifdef _DEBUG
+		printf("Rodando em modo debug.\n");
+	#endif
 
 	printf("Executando o simulador...\n");
 	printf("Arquivo de entrada: %s\n", addressFile);
 	printf("Tamanho da memoria fisica: %d MB\n", physicalMemorySize);
 	printf("Tamanho das paginas: %d KB\n", pageSize);
 	printf("Alg de substituicao: %s\n", replacementAlg);
-	
+
 	int pageTableSize = getPageTableSize(pageSize);
 	int pageFrameSize = getPageFrameSize(pageSize, physicalMemorySize);
 	printf("Page table size: %d\n", pageTableSize);
@@ -77,79 +198,25 @@ int main(int argc, char *argv[])
 	FILE* fp = fopen(addressFile, "r");
 	if(fp == NULL)
 	{
-		printf("File not found!");
+		printf("Arquivo não encontrado!");
 		exit(2);
 	}
 
 	int qntAddr = 0;
-	
+
 
 	for(i=0; i<pageTableSize; i++)
 	{
 		pageTable[i] = -1;
 	}
 
-	while(fscanf(fp, "%x %c", &addr, &rw) == 2)
+	if(strcmp(replacementAlg, "NRU")==0)
+	{}
+	else if(strcmp(replacementAlg, "LRU")==0)
 	{
-		int newPageIndex = getPageIndex(addr, pageSize);
-		int indexNewPageFrame = pageTable[newPageIndex];
-		int oldPageIndex;
-		
-
-		PageFrame newPage;
-
-		if(indexNewPageFrame == -1)
-		{
-
-			if(pagesEmpty != 0)
-			{
-				newPage.lastAcessed = time;
-
-				if(rw == 'W')
-				{
-					newPage.R = 0;
-					newPage.M = 1;
-				}
-				else
-				{
-					newPage.R = 1;
-					newPage.M = 0;
-				}
-
-				pagesEmpty--;
-				oldPageIndex = pageFrameSize - pagesEmpty;
-				pageFrame[oldPageIndex] = newPage;
-				pageTable[newPageIndex] = oldPageIndex;	
-			}
-			else
-			{
-				if(strcmp(replacementAlg, "NRU")==0)
-				{
-
-				}
-				else if(strcmp(replacementAlg, "LRU")==0)
-				{}
-				else if(strcmp(replacementAlg, "NOVO") == 0)
-				{}
-			}
-		}
-		else
-		{
-			if(rw == 'W')
-			{
-				pageFrame[indexNewPageFrame].M = 1;
-			}
-			else
-			{
-				pageFrame[indexNewPageFrame].R = 1;
-			}
-			pageFrame[indexNewPageFrame].lastAcessed = time;
-		}
-
-		time++;
-		qntAddr++;
+		LRU(pageTable, pageFrame, pageFrameSize, pageSize, deltaTime, fp);
 	}
-
-	printf("Number of adresses: %d\n", qntAddr);
+	else if(strcmp(replacementAlg, "NOVO") == 0)
+	{}
 
 }
