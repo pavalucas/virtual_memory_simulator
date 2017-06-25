0684eebf770a1e616353296339c02518a23cbad6
HandelSM <hscholzem@gmail.com>
2017-06-25 03:42:35 -0300

    NRU algorithm implemented

--- main.c
+++ main.c
@@ -38,6 +38,132 @@ int getPageFrameSize(int pageSize, int physMemSize)
 	return ((physMemSize * 1024) / pageSize);
 }
 
+void NRU(int* pageTable, PageFrame* pageFrame, int pageFrameSize, int pageSize, int deltaTime, FILE* fp)
+{
+	unsigned int addr;
+	char rw;
+	int i;
+	int qtPageFault = 0;
+	int qtPhysMemAcess = 0;
+	int pagesEmpty = pageFrameSize;
+	unsigned long long time = 1;
+
+	while(fscanf(fp, "%x %c", &addr, &rw) == 2)
+	{
+		int newPageIndex = getPageIndex(addr, pageSize);
+		int indexNewPageFrame = pageTable[newPageIndex];
+		int oldPageIndex;
+
+		PageFrame newPage;
+
+		// Setting all R bit to zero
+		if(time % deltaTime == 0)
+		{
+			for(i = 0; i < pageFrameSize; i++)
+			{
+				pageFrame[i].R = 0;
+			}
+		}
+
+		if(indexNewPageFrame == -1)
+		{
+			if(pagesEmpty != 0)
+			{
+				newPage.lastAcessed = time;
+
+				if(rw == 'W')
+					newPage.M = 1;
+				else
+					newPage.M = 0;
+				newPage.R = 1;
+
+				oldPageIndex = pageFrameSize - pagesEmpty;
+				pagesEmpty--;
+				pageFrame[oldPageIndex] = newPage;
+				pageTable[newPageIndex] = oldPageIndex;
+				#ifdef _DEBUG
+					printf("Nova page frame %d preenchida\n", oldPageIndex);
+				#endif
+			}
+			else
+			{
+				PageFrame choosed = pageFrame[0];
+				int choosedIndex = 0;
+
+				for(i = 1; i < pageFrameSize; i++)
+				{
+					if(choosed.R == 0)
+					{
+						if(choosed.M == 0)
+						{
+							break;
+						}
+						else if(pageFrame[i].M == 0)
+						{
+							choosed = pageFrame[i];
+							choosedIndex = i;
+						}
+					}
+					else
+					{
+						if(pageFrame[i].R == 0)
+						{
+							choosed = pageFrame[i];
+							choosedIndex = i;
+						}
+						else if(pageFrame[i].R == 0) 
+						{
+							choosed = pageFrame[i];
+							choosedIndex = i;														
+						}
+					}
+				}
+
+				#ifdef _DEBUG
+					printf("Page fault!\n");
+					printf("Pagina %d sera retirada.\n", choosedIndex);
+				#endif
+
+				if(choosed.M == 1)
+				{
+					#ifdef _DEBUG
+						printf("Acesso a memoria fisica!\n");
+					#endif
+					qtPhysMemAcess++;
+				}
+
+				if(rw == 'W')
+					pageFrame[choosedIndex].M = 1;
+				else
+					pageFrame[choosedIndex].M = 0;
+				pageFrame[choosedIndex].R = 1;
+				pageFrame[choosedIndex].lastAcessed = time;
+
+				pageTable[newPageIndex] = choosedIndex;
+
+				qtPageFault++;
+			}
+		}
+		else
+		{
+			#ifdef _DEBUG
+				printf("Page %d hit!\n", indexNewPageFrame);
+			#endif
+
+			if(rw == 'W')
+				pageFrame[indexNewPageFrame].M = 1;
+			pageFrame[indexNewPageFrame].R = 1;
+
+			pageFrame[indexNewPageFrame].lastAcessed = time;
+		}
+
+		time++;
+	}
+	printf("Numero de Faltas de Páginas: %d\n", qtPageFault);
+	printf("Numero de Paginas escritas: %d\n", qtPhysMemAcess);
+	
+}
+
 void LRU(int* pageTable, PageFrame* pageFrame, int pageFrameSize, int pageSize, int deltaTime, FILE* fp)
 {
 	unsigned int addr;
@@ -211,7 +337,9 @@ int main(int argc, char *argv[])
 	}
 
 	if(strcmp(replacementAlg, "NRU")==0)
-	{}
+	{
+		NRU(pageTable, pageFrame, pageFrameSize, pageSize, deltaTime, fp);
+	}
 	else if(strcmp(replacementAlg, "LRU")==0)
 	{
 		LRU(pageTable, pageFrame, pageFrameSize, pageSize, deltaTime, fp);
